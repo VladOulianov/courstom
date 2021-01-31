5c337d54d9e965f6abad4699b921bf3622824ac3
VladOulianov <64905404+VladOulianov@users.noreply.github.com>
2021-01-31 18:22:50 +0100

    init

---
+++ app.js
@@ -0,0 +1,18 @@
+const nombre = 2;
+const cheval = "cheval";
+
+const tableau = [
+    2,
+    3,
+    85,
+    48
+]
+console.log('element : '+ tableau);
+
+console.log(nombre +' '+ cheval);
+console.log(nombre);
+
+
+const mail = document.getElementById('mail').values
+
+console.log(mail);
